520f7c9feb462ae44bf5cfb0cd2b2af406062bc7
Amos Thando Mpofu <56162910+theliteprogrammer@users.noreply.github.com>
2023-04-02 14:59:18 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IllegalAccessException {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.insert(90);
+        System.out.println("Before:");
+        numbers.print();
+        numbers.removeAt(4);
+        System.out.println("After removing at index 4:");
+        numbers.print();
     }
 }
